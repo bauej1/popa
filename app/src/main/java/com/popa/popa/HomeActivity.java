e97612b3776b668312e11c2c7a9d33606a700ece
Bauer Jan Gabriel <jan.bauer@bfh.ch>
2018-10-15 19:07:22 +0200

    created homescreen with all resources

--- app/src/main/java/com/popa/popa/HomeActivity.java
+++ app/src/main/java/com/popa/popa/HomeActivity.java
@@ -2,12 +2,40 @@ package com.popa.popa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
+
+    /*ImageButton bStatistics = (ImageButton) findViewById(R.id.bStatistics);
+    ImageButton bDiary = (ImageButton) findViewById(R.id.bDiary);
+    ImageButton bInfo = (ImageButton) findViewById(R.id.bInfo);
+    ImageButton bSettings = (ImageButton) findViewById(R.id.bSettings);
+    ImageButton bPet = (ImageButton) findViewById(R.id.bPet);*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+/*
+        bStatistics.setOnClickListener((View v) -> {
+
+        });
+
+        bDiary.setOnClickListener((View v) -> {
+
+        });
+
+        bInfo.setOnClickListener((View v) -> {
+
+        });
+
+        bSettings.setOnClickListener((View v) -> {
+
+        });
+
+        bPet.setOnClickListener((View v) -> {
+
+        });*/
     }
 }
