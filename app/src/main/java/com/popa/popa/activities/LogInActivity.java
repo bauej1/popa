88a85f06ff22ad84e937d38dcab3707cd0692576
leameierx <meier.lea@icloud.com>
2018-11-02 15:42:52 +0100

    added activity for login

--- app/src/main/java/com/popa/popa/activities/LogInActivity.java
+++ app/src/main/java/com/popa/popa/activities/LogInActivity.java
@@ -1,4 +1,14 @@
 package com.popa.popa.activities;
 
-public class LogInActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.popa.popa.R;
+
+public class LogInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
 }
