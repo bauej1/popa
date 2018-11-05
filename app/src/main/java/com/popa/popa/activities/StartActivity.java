4bd0b00a9f2764d89e1301c017f5814576338d8c
leameierx <meier.lea@icloud.com>
2018-11-05 13:39:25 +0100

    added gender view and start activity

---
+++ app/src/main/java/com/popa/popa/activities/StartActivity.java
@@ -0,0 +1,53 @@
+package com.popa.popa.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.popa.popa.R;
+
+import pl.droidsonroids.gif.GifImageView;
+
+public class StartActivity extends AppCompatActivity {
+
+    ImageButton buttonGenderFemale;
+    ImageButton buttonGenderMale;
+    TextView textSwipe;
+    GifImageView gifSwipe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.l_gender);
+
+        buttonGenderFemale = (ImageButton) findViewById(R.id.genderF);
+        buttonGenderMale = (ImageButton) findViewById(R.id.genderM);
+        textSwipe = (TextView) findViewById(R.id.swipeText);
+        gifSwipe = (GifImageView) findViewById(R.id.swipeGif);
+
+        textSwipe.setVisibility(View.INVISIBLE);
+        gifSwipe.setVisibility(View.INVISIBLE);
+
+        buttonGenderFemale.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+                buttonGenderMale.setImageAlpha(63);
+                buttonGenderFemale.setImageAlpha(255);
+                textSwipe.setVisibility(View.VISIBLE);
+                gifSwipe.setVisibility(View.VISIBLE);
+            }
+        });
+
+        buttonGenderMale.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+                buttonGenderFemale.setImageAlpha(63);
+                buttonGenderMale.setImageAlpha(255);
+                textSwipe.setVisibility(View.VISIBLE);
+                gifSwipe.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
