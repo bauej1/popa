5ccf59e765903bf380849203406fe87b1b9131b4
leameierx <meier.lea@icloud.com>
2018-11-05 16:38:34 +0100

    added mood and pain view

--- app/src/main/java/com/popa/popa/activities/LogInActivity.java
+++ app/src/main/java/com/popa/popa/activities/LogInActivity.java
@@ -3,12 +3,15 @@ package com.popa.popa.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+
 import com.popa.popa.R;
 
 public class LogInActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.l_painlocation);
+        setContentView(R.layout.l_paintoday);
     }
 }
