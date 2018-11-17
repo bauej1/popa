f25b294ab8fa651a53d0ae6c66892ce29e1f6b49
aescj1 <janphilippe.aeschimann@students.bfh.ch>
2018-11-17 23:39:23 +0100

    Added Wear OS

---
+++ popawear/src/main/java/com/example/popawear/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.popawear;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends WearableActivity {
+
+    private final static String TAG = "Wear MainActivity";
+    private TextView mTextView;
+    Button myButton;
+    int num = 0;
+    String datapath = "/message_path";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+        myButton = findViewById(R.id.myButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "Hello device " + num;
+                //Requires a new thread to avoid blocking the UI
+                new SendThread(datapath, message).start();
+                num++;
+            }
+        });
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+
+
+    //This actually sends the message to the wearable device.
+    class SendThread extends Thread {
+        String path;
+        String message;
+
+        //constructor
+        SendThread(String p, String msg) {
+            path = p;
+            message = msg;
+        }
+        //sends the message via the thread.  this will send to all wearables connected, but
+        //since there is (should only?) be one, so no problem.
+        public void run() {
+            //first get all the nodes, ie connected wearable devices.
+            Task<List<Node>> nodeListTask =
+                    Wearable.getNodeClient(getApplicationContext()).getConnectedNodes();
+            try {
+                // Block on a task and get the result synchronously (because this is on a background
+                // thread).
+                List<Node> nodes = Tasks.await(nodeListTask);
+
+                //Now send the message to each device.
+                for (Node node : nodes) {
+                    Task<Integer> sendMessageTask =
+                            Wearable.getMessageClient(MainActivity.this).sendMessage(node.getId(), path, message.getBytes());
+
+                    try {
+                        // Block on a task and get the result synchronously (because this is on a background
+                        // thread).
+                        Integer result = Tasks.await(sendMessageTask);
+                        Log.v(TAG, "SendThread: message send to " + node.getDisplayName());
+
+                    } catch (ExecutionException exception) {
+                        Log.e(TAG, "Task failed: " + exception);
+
+                    } catch (InterruptedException exception) {
+                        Log.e(TAG, "Interrupt occurred: " + exception);
+                    }
+
+                }
+
+            } catch (ExecutionException exception) {
+                Log.e(TAG, "Task failed: " + exception);
+
+            } catch (InterruptedException exception) {
+                Log.e(TAG, "Interrupt occurred: " + exception);
+            }
+        }
+    }
+}
