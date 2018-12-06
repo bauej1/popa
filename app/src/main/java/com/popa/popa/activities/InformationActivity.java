eebd2316ea7381f79fe2f1d66ac5fa937c02decf
leameierx <meier.lea@icloud.com>
2018-12-06 19:44:44 +0100

    added tips to improve your posture to informationView

--- app/src/main/java/com/popa/popa/activities/InformationActivity.java
+++ app/src/main/java/com/popa/popa/activities/InformationActivity.java
@@ -1,7 +1,11 @@
 package com.popa.popa.activities;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 
 import com.popa.popa.R;
@@ -21,6 +25,16 @@ public class InformationActivity extends AppCompatActivity {
         tips = findViewById(R.id.bTips);
         clothes = findViewById(R.id.bClothes);
 
-
+        tips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View tipsView = inflater.inflate(R.layout.i_tips,null, false);
+                AlertDialog.Builder builder = new AlertDialog.Builder(InformationActivity.this);
+                builder.setView(tipsView);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
     }
 }
